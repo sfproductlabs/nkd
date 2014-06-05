0222d1740b2c02ff99ba5dffb9239250987a8559
Andrew Grosser <dioptre@gmail.com>
2014-06-05 12:27:54 +1000

    added ie checks

--- src/main/NKD.Orchard/src/Orchard.Web/Modules/NKD/Scripts/NKD.utils.js
+++ src/main/NKD.Orchard/src/Orchard.Web/Modules/NKD/Scripts/NKD.utils.js
@@ -47,3 +47,11 @@ String.prototype.regexLastIndexOf = function (regex, startpos) {
     }
     return lastIndexOf;
 }
+
+function GetExplorerVersion() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer, return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)));
+    return 0;
+}
